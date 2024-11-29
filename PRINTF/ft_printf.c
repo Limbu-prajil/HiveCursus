1441a222d4dba33421dd30e3e18c195145b120c6
pra-lim <81345206+Limbu-prajil@users.noreply.github.com>
2024-11-30 00:23:14 +0200

    write function testing for errors

--- PRINTF/ft_printf.c
+++ PRINTF/ft_printf.c
@@ -7,63 +7,53 @@ static int	ft_verify(char c, va_list args)
 	size = 0;
 	if (c == 'c')
 		size = ft_printf_char(va_arg(args, int));
-		return (size);
 	else if (c == 's')
 		size = ft_printf_string(va_arg(args, char *));
-		return (size);
 	else if (c == 'd' || c == 'i')
 		size = ft_printf_nbr(va_arg(args, int));
-		return (size);
 	else if (c == 'u')
 		size = ft_printf_unbr(va_arg(args, unsigned int));
-		return (size);
 	else if (c == 'x')
 		size = ft_printf_hexlow(va_arg(args, unsigned int));
-		return (size);
 	else if (c == 'X')
 		size = ft_printf_hexupp(va_arg(args, unsigned int));
-		return (size);
 	else if (c == '%')
 		size = ft_putchar_fd('%', 1);
-		return (size);
 	else if (c == 'p')
 		size = ft_printf_pointer(va_arg(args, void *));
-	else
-		return (size);
+	return (size);
 }
 
-static int	ft_check()
-
-static int	ft_write(const char *s, va_list args)
+static int	ft_check(const char s)
 {
-	unsigned int	i;
-	unsigned int	size;
+	if (!s || s == ' ' || s != 'c' || s != 's' || s != 'd'
+		|| s != 'i' || s != 'u' || s != 'x' || s != 'X'
+				|| s != '%' || s != 'p')
+		return (-1);
+}
 
-	i = 0;
+static int	ft_write(const char *str, va_list args)
+{
+	size_t		size;
+
 	size = 0;
-	rtn = 0;
-	while (s[i])
+	while (str)
 	{
-		if (s[i] == '%')
+		if (str == '%')
 		{
-			s = s[i + 1];
-			if (!s || s == ' ' || s != 'c' || s != 's' || s != 'd'
-				|| s != 'i' || s != 'u' || s != 'x' || s != 'X'
-					|| s != '%' || s != 'p')
+			if (ft_check(str + 1) == -1)
 				return (-1);
-			rtn = ft_verify(s[i + 1], args);
-			if (rtn == -1)
+			if (ft_verify(str + 1, args) == -1)
 				return (-1);
-			size += rtn;
-			i += 2;
+			size += ft_verify(str - 1, args);
+			str += 2;
 		}
 		else
 		{
-			rtn = ft_putchar_fd(s[i], 1);
-			if (rtn == -1)
+			if (ft_putchar_fd(str, 1) == -1)
 				return (-1);
 			size++;
-			i++;
+			str++;
 		}
 	}
 	return (size);
