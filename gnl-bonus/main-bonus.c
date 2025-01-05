f52976dee401cba4d070447b50e6f0ba60e3c2a1
pra-lim <81345206+Limbu-prajil@users.noreply.github.com>
2025-01-05 12:40:09 +0200

    few changes to gnl bonus

--- gnl-bonus/main-bonus.c
+++ gnl-bonus/main-bonus.c
@@ -5,12 +5,13 @@
 int main()
 {
     // Open multiple file descriptors
-    int fd1 = open("./example.txt", O_RDONLY);
-    int fd2 = open("./twinkle.txt", O_RDONLY);
-    int fd3 = open("./jhonny.txt", O_RDONLY);
+    int fd3 = open("./example.txt", O_RDONLY);
+    int fd4 = open("./example1.txt", O_RDONLY);
+    int fd5 = open("./example2.txt", O_RDONLY);
+    printf("%d %d %d\n", fd3, fd4, fd5);
     char *line;
 
-    if (fd1 < 0 || fd2 < 0 || fd3 < 0)
+    if (fd3 < 0 || fd4 < 0 || fd5 < 0)
     {
         printf("Error opening files\n");
         return (1);
@@ -19,22 +20,6 @@ int main()
     // Read lines alternately from each file descriptor
     for (int i = 0; i < 11; i++) // Adjust the loop count as needed
     {
-        // Read from fd1
-        line = get_next_line(fd1);
-        if (line)
-        {
-            printf("FD1: %s", line);
-            free(line);
-        }
-
-        // Read from fd2
-        line = get_next_line(fd2);
-        if (line)
-        {
-            printf("FD2: %s", line);
-            free(line);
-        }
-
         // Read from fd3
         line = get_next_line(fd3);
         if (line)
@@ -42,12 +27,28 @@ int main()
             printf("FD3: %s", line);
             free(line);
         }
+
+        // Read from fd4
+        line = get_next_line(fd4);
+        if (line)
+        {
+            printf("FD4: %s", line);
+            free(line);
+        }
+
+        // Read from fd5
+        line = get_next_line(fd5);
+        if (line)
+        {
+            printf("FD5: %s", line);
+            free(line);
+        }
     }
 
     // Close all file descriptors
-    close(fd1);
-    close(fd2);
     close(fd3);
+    close(fd4);
+    close(fd5);
 
     return 0;
 }
