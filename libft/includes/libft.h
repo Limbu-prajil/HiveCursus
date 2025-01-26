b8245707b0021010b605ce19976d1d6333af9b05
pra-lim <81345206+Limbu-prajil@users.noreply.github.com>
2025-01-26 02:33:42 +0200

    structure for all project files

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -16,19 +16,51 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-size_t		ft_strlen(const char *s);
-size_t		ft_strlcpy(char *dst, const char *src, size_t size);
-size_t		ft_strlcat(char *dst, const char *src, size_t size);
-void		ft_putchar_fd(char c, int fd);
-void		ft_putstr_fd(char *s, int fd);
-void		ft_putnbr_fd(int n, int fd);
-void		ft_putendl_fd(char *s, int fd);
-void		*ft_memset(void *s, int c, size_t n);
+typedef struct s_list
+{
+    cvoid         *content;
+    struct s_list *next;
+} t_list;
+
+/* CHECK */
+int			ft_isalpha(int c);
+int			ft_isdigit(int c);
+int			ft_isalnum(int c);
+int			ft_isprint(int c);
+int			ft_isascii(int c);
+int			ft_toupper(int c);
+int			ft_tolower(int c);
+
+/* LIST */
+void	ft_lstadd_back(t_list **lst, t_list *newl);
+void	ft_lstadd_front(t_list **lst, t_list *newl);
+void	ft_lstclear(t_list **lst, void (*del)(void*));
+void	ft_lstdelone(t_list *lst, void (*del)(void*));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstlast(t_list *lst);
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list	*ft_lstnew(void *content);
+int		ft_lstsize(t_list *lst);
+
+/* MEMORY */
 void		ft_bzero(void *s, size_t n);
+void		*ft_memset(void *s, int c, size_t n);
+int			ft_memcmp(const void *s1, const void *s2, size_t n);
 void		*ft_calloc(size_t nmemb, size_t size);
 void		*ft_memcpy(void *dest, const void *src, size_t n);
 void		*ft_memmove(void *dest, const void *src, size_t n);
 void		*ft_memchr(const void *s, int c, size_t n);
+
+/* PRINT */
+void		ft_putchar_fd(char c, int fd);
+void		ft_putstr_fd(char *s, int fd);
+void		ft_putnbr_fd(int n, int fd);
+void		ft_putendl_fd(char *s, int fd);
+
+/* STRING */
+size_t		ft_strlen(const char *s);
+size_t		ft_strlcpy(char *dst, const char *src, size_t size);
+size_t		ft_strlcat(char *dst, const char *src, size_t size);
 void		ft_striteri(char *s, void (*f)(unsigned int, char*));
 char		*ft_strrchr(const char *s, int c);
 char		*ft_strchr(const char *s, int c);
@@ -41,14 +73,6 @@ char		*ft_strtrim(char const *s1, char const *set);
 char		**ft_split(char const *s, char c);
 char		*ft_itoa(int n);
 int			ft_atoi(const char *c);
-int			ft_memcmp(const void *s1, const void *s2, size_t n);
-int			ft_isalpha(int c);
-int			ft_isdigit(int c);
-int			ft_isalnum(int c);
-int			ft_isprint(int c);
-int			ft_isascii(int c);
-int			ft_toupper(int c);
-int			ft_tolower(int c);
 int			ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
