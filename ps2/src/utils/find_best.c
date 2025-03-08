fd2772a55088dd94668c5558c0489b6783fe38cf
pra-lim <81345206+Limbu-prajil@users.noreply.github.com>
2025-03-09 00:49:43 +0200

    files that works

--- ps2/src/utils/find_best.c
+++ ps2/src/utils/find_best.c
@@ -4,18 +4,23 @@ int	best_combo(t_data *stacks, int depth_a, int depth_b)
 {
 	if (depth_a == 0 && depth_b == 0)
 		return (0);
+
 	else if (depth_a > ft_lstsize(stacks->a) / 2 
 		&& depth_b > ft_lstsize(stacks->b) / 2)
 		return (rra_rrb_calc(stacks, depth_a, depth_b));
+	
 	else if (depth_a <= ft_lstsize(stacks->a) / 2 
 		&& depth_b <= ft_lstsize(stacks->b) / 2)
 		return (ra_rb_calc(depth_a, depth_b));
+	
 	else if (depth_a <= ft_lstsize(stacks->a) / 2 
 		&& depth_b > ft_lstsize(stacks->b) / 2)
 		return (ra_rrb_calc(stacks, depth_a, depth_b));
+	
 	else if (depth_a > ft_lstsize(stacks->a) / 2 
 		&& depth_b <= ft_lstsize(stacks->b) / 2)
 		return (rra_rb_calc(stacks, depth_a, depth_b));
+	
 	return (0);
 }
 
